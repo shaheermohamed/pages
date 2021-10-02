dbe40f2c1e98812170c33286dd99980e4a83ea68
shaheer <mohamedshaheer08@gmail.com>
2021-10-02 15:23:00 +0530

    home and about page view ,testing added

--- pages/urls.py
+++ pages/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import HomePageView,Contactpageview
 
 urlpatterns = [
-    path('home/', HomePageView.as_view(), name='Home'),
+    path('', HomePageView.as_view(), name='Home'),
     path('about/',Contactpageview.as_view(), name='about'),
 ]
